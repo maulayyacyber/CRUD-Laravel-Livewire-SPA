88d1b419bcb60f902523b6fcaf3f0fc0f9d8471f
maulayyacyber <ridaulmaulayya@gmail.com>
2020-05-19 09:57:20 +0700

    rename title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Livewire SPA</title>
+    <title>Laravel Livewire SPA - SantriKoding.com</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <livewire:styles/>
     <livewire:scripts/>
